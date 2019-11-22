ce68dc4a7be29c898fe627c12a534315188d21f7
Feysel <feyselmubarek@gmail.com>
2019-11-22 21:00:19 +0300

    InsertionSort completed

---
+++ Lesson 4/InsertionSort/src/app/App.java
@@ -0,0 +1,29 @@
+package app;
+
+import java.util.ArrayList;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        RandomGenerator generator = new RandomGenerator();
+        performInsertionSort(generator.getRandomNumber());
+    }
+
+    public static void performInsertionSort(ArrayList<Integer> arrayList) {
+        for (int i = 0; i < arrayList.size(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (arrayList.get(i) < arrayList.get(j)) {
+                    int num = arrayList.get(i);
+                    arrayList.remove(i);
+                    arrayList.add(j, num);
+                }
+            }
+        }
+
+        System.out.print("[ ");
+        for (Integer integer : arrayList) {
+            System.out.print(integer + ", ");
+        }
+        System.out.print(" ]");
+
+    }
+}
---
+++ Lesson 4/InsertionSort/src/app/RandomGenerator.java
@@ -0,0 +1,32 @@
+package app;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+/**
+ * RandomGenerator
+ */
+public class RandomGenerator {
+    Random random;
+
+    public RandomGenerator() {
+        random = new Random();
+    }
+
+    public ArrayList<Integer> getRandomNumber() {
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+        while (arrayList.size() < 10000) {
+            int number = random.nextInt(10000);
+            if (!map.containsKey(number)) {
+                arrayList.add(number);
+                map.put(number, number);
+            }
+        }
+
+        return arrayList;
+    }
+}
