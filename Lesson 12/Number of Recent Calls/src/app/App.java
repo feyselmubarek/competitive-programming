8bd7e4cf9a1ed452e94d14935b445654f8718525
Feysel <feyselmubarek@gmail.com>
2019-12-09 09:20:53 +0300

    Number of recent calls completed

---
+++ Lesson 12/Number of Recent Calls/src/app/App.java
@@ -0,0 +1,12 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        RecentCounter recentCounter = new RecentCounter();
+        System.out.println(recentCounter.ping(1));
+        System.out.println(recentCounter.ping(100));
+        System.out.println(recentCounter.ping(3001));
+        System.out.println(recentCounter.ping(1000000000));
+    }
+}
---
+++ Lesson 12/Number of Recent Calls/src/app/RecentCounter.java
@@ -0,0 +1,27 @@
+package app;
+
+import java.util.ArrayList;
+
+public class RecentCounter {
+    ArrayList<Integer> timeStamps;
+
+    public RecentCounter() {
+        timeStamps = new ArrayList<>();
+    }
+
+    public int ping(int t) {
+        timeStamps.add(t);
+
+        int min = t - 3000;
+        int counter = 0;
+
+        for (int i = timeStamps.size() - 1; i >= 0; i--) {
+            int time = timeStamps.get(i);
+            if (time >= min && time <= t) {
+                counter++;
+            }
+        }
+
+        return counter;
+    }
+}
