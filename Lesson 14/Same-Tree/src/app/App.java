cf2dfad954270106f2d68583a0ba29fdae7f36ff
Feysel <feyselmubarek@gmail.com>
2019-12-10 19:22:57 +0300

    Same tree completed

--- Lesson 14/Same-Tree/src/app/App.java
+++ Lesson 14/Same-Tree/src/app/App.java
@@ -2,6 +2,26 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        TreeNode treeNode1 = new TreeNode(1);
+        treeNode1.left = new TreeNode(3);
+        treeNode1.right = new TreeNode(2);
+
+        treeNode1.left.left = new TreeNode(5);
+        // treeNode1.left.right = new TreeNode(7);
+
+        // treeNode.right.right = new TreeNode(18);
+        // treeNode.right.left = new TreeNode(3);
+
+        TreeNode treeNode = new TreeNode(1);
+        treeNode.left = new TreeNode(3);
+        treeNode.right = new TreeNode(2);
+
+        treeNode.left.left = new TreeNode(5);
+        // treeNode.left.right = new TreeNode(4);
+
+        // treeNode.right.right = new TreeNode(7);
+        // treeNode.right.left = new TreeNode(3);
+
+        System.out.println(new Solution().isSameTree(treeNode1, treeNode));
     }
 }
---
+++ Lesson 14/Same-Tree/src/app/Solution.java
@@ -0,0 +1,31 @@
+package app;
+
+public class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        boolean leftNodeBool = true, rightNodeBool = true;
+
+        if (p == null && q == null) {
+            return true;
+        }
+        if ((p != null && q == null) || (p == null && q != null)) {
+            return false;
+        }
+
+        if (p.val != q.val) {
+            return false;
+        }
+
+        TreeNode l1, l2, r1, r2;
+
+        l1 = p != null ? p.left : null;
+        l2 = q != null ? q.left : null;
+
+        r1 = p != null ? p.right : null;
+        r2 = p != null ? q.right : null;
+
+        leftNodeBool = isSameTree(l1, l2);
+        rightNodeBool = isSameTree(r1, r2);
+
+        return leftNodeBool && rightNodeBool;
+    }
+}
