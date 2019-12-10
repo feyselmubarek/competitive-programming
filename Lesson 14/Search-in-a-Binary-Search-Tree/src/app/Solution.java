a0ccd24176e727d575d937ddf984f0d7778bf318
Feysel <feyselmubarek@gmail.com>
2019-12-10 18:18:56 +0300

    Search binary tree completed

--- Lesson 14/Search-in-a-Binary-Search-Tree/src/app/Solution.java
+++ Lesson 14/Search-in-a-Binary-Search-Tree/src/app/Solution.java
@@ -2,6 +2,28 @@ package app;
 
 public class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
+        TreeNode node = null, rightNode = null;
 
+        if (root == null) {
+            return null;
+        }
+
+        if (root.val == val) {
+            return root;
+        }
+
+        if (root.left != null) {
+            node = searchBST(root.left, val);
+        }
+
+        if (root.right != null) {
+            rightNode = searchBST(root.right, val);
+        }
+
+        if (node != null) {
+            return node;
+        }
+
+        return rightNode;
     }
 }
