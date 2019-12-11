0a044ee15b8816573d6465ffe353f9baafd154a7
Feysel <feyselmubarek@gmail.com>
2019-12-11 14:16:05 +0300

    insert node into BTS completed

--- Lesson 14/Insert-into-a-Binary-Search-Tree/src/app/App.java
+++ Lesson 14/Insert-into-a-Binary-Search-Tree/src/app/App.java
@@ -2,6 +2,31 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        TreeNode treeNode = new TreeNode(3);
+        treeNode.left = new TreeNode(2);
+        treeNode.right = new TreeNode(8);
+
+        treeNode.left.left = new TreeNode(1);
+        treeNode.left.right = new TreeNode(2);
+
+        treeNode.right.right = new TreeNode(16);
+        treeNode.right.left = new TreeNode(5);
+
+        TreeNode n = new Solution().insertIntoBST(treeNode, 6);
+
+        print(n);
+
+    }
+
+    public static void print(TreeNode n) {
+        System.out.println(n.val + "=> node ");
+
+        if (n.left != null) {
+            print(n.left);
+        }
+
+        if (n.right != null) {
+            print(n.right);
+        }
     }
 }
--- Lesson 14/Insert-into-a-Binary-Search-Tree/src/app/Solution.java
+++ Lesson 14/Insert-into-a-Binary-Search-Tree/src/app/Solution.java
@@ -2,6 +2,28 @@ package app;
 
 class Solution {
     public TreeNode insertIntoBST(TreeNode root, int val) {
+        TreeNode leftNode = null, rightNode = null;
 
+        if (root == null) {
+            return new TreeNode(val);
+        }
+
+        if (root.val >= val && root != null) {
+            leftNode = insertIntoBST(root.left, val);
+        }
+
+        if (root.val <= val && root != null) {
+            rightNode = insertIntoBST(root.right, val);
+        }
+
+        if (leftNode != null) {
+            root.left = leftNode;
+        }
+
+        if (rightNode != null) {
+            root.right = rightNode;
+        }
+
+        return root;
     }
 }
