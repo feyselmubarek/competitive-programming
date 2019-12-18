0a5a0eed56b75435c4b2769b2f8c76f116bff712
Feysel <feyselmubarek@gmail.com>
2019-12-19 00:07:58 +0300

    Reorgnize string completed

--- Lesson 07/Reorganize-String/src/app/Solution.java
+++ Lesson 07/Reorganize-String/src/app/Solution.java
@@ -3,8 +3,42 @@ package app;
 public class Solution {
 
     public String reorganizeString(String S) {
+        int[] lettersOccur = new int[26];
 
-        return "a";
+        for (int i = 0; i < S.length(); i++) {
+            lettersOccur[(int) S.charAt(i) - 97] += 1;
+        }
+
+        StringBuilder stringBuilder = new StringBuilder(S);
+        int maxIndex = getMaxIndexWithoutCurrent(lettersOccur, ((int)'A'));
+
+        for (int i = 0; i < S.length(); i++) {
+            stringBuilder.setCharAt(i, (char)(maxIndex + 97));
+            int curMaxIndex = getMaxIndexWithoutCurrent(lettersOccur, maxIndex);
+    
+            if (curMaxIndex == maxIndex && i != S.length() - 1) {
+                return "";    
+            }
+            
+            maxIndex = curMaxIndex;
+        }
+
+        return stringBuilder.toString();
+    }
+
+    public int getMaxIndexWithoutCurrent(int[] list, int current) {
+        int max = 0, index = current;
+
+        for (int i = 0; i < list.length; i++) {
+            if (list[i] >= max && i != current && list[i] != 0) {
+                max = list[i];
+                index = i;
+            }
+        }
+
+        list[index] = list[index] - 1;
+
+        return index;
     }
 
 }
