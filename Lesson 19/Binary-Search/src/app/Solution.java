7758cba6f1430aa7cfe6ab8dbfd028ff5801b942
Feysel <feyselmubarek@gmail.com>
2019-12-23 09:56:39 +0300

    Modifing Binary search

--- Lesson 19/Binary-Search/src/app/Solution.java
+++ Lesson 19/Binary-Search/src/app/Solution.java
@@ -3,25 +3,19 @@ package app;
 public class Solution {
     public int search(int[] nums, int target) {
         int i = 0, j = nums.length - 1;
-        int mid = ((i + j) + 1) / 2;
+        int mid = 0;
 
-        while (j - i > 1) {
+        while (i <= j) {
+            mid = i == j ? i : ((i + j) - 1) / 2;
+
             if (nums[mid] > target) {
-                j = mid;
+                j = mid - 1;
             } else if (nums[mid] < target) {
-                i = mid;
+                i = mid + 1;
             } else {
                 return mid;
             }
-            mid = ((i + j) + 1) / 2;
         }
-
-        if (nums[i] == target) {
-            return i;
-        } else if (nums[j] == target) {
-            return j;
-        }
-
         return -1;
     }
 }
