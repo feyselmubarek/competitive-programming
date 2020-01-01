5a3faada793c2632786a13ddfcaa35cd4f191842
Feysel <feyselmubarek@gmail.com>
2020-01-01 18:42:41 +0300

    Modification using custom priority queue

--- Lesson 19/Top-K-Frequent-Elements/src/app/App.java
+++ Lesson 19/Top-K-Frequent-Elements/src/app/App.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        List<Integer> res = new Solution().topKFrequent(new int[] { 1, 2, 1, 2, 3, 1 }, 2);
+        List<Integer> res = new Solution().topKFrequent(new int[] { 4, 4, 4, 2, 2, 3, 3 }, 3);
 
         System.out.println();
         for (Integer integer : res) {
--- Lesson 19/Top-K-Frequent-Elements/src/app/Solution.java
+++ Lesson 19/Top-K-Frequent-Elements/src/app/Solution.java
@@ -3,28 +3,30 @@ package app;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.PriorityQueue;
 
 public class Solution {
     public List<Integer> topKFrequent(int[] nums, int k) {
         HashMap<Integer, Integer> occuranceMap = new HashMap<Integer, Integer>();
 
-        for (int last = 0; last < nums.length; last++) {
-            if (occuranceMap.containsKey(nums[last]))
-                occuranceMap.put(nums[last], occuranceMap.get(nums[last]) + 1);
-            else
-                occuranceMap.put(nums[last], 1);
+        for (int first = 0; first < nums.length; first++) {
+            if (occuranceMap.containsKey(nums[first])) {
+                occuranceMap.put(nums[first], occuranceMap.get(nums[first]) + 1);
+            } else {
+                occuranceMap.put(nums[first], 1);
+            }
         }
-        PriorityQueue<Pair> priorityQueue = new PriorityQueue<Pair>(occuranceMap.size(), (a, b) -> (b.y - a.y));
 
-        for (int last : occuranceMap.keySet())
-            priorityQueue.add(new Pair(last, occuranceMap.get(last)));
+        MyPriorityQueue priorityQueue = new MyPriorityQueue(occuranceMap.size());
+
+        for (int first : occuranceMap.keySet()) {
+            priorityQueue.insertWithPriority(new Pair(first, occuranceMap.get(first)));
+        }
 
         List<Integer> result = new ArrayList<>();
 
         for (int i = 0; i < k; i++) {
-            Pair top = priorityQueue.remove();
-            result.add(top.x);
+            Pair top = priorityQueue.pullHighestPriority();
+            result.add(top.number);
         }
 
         return result;
@@ -32,31 +34,32 @@ public class Solution {
 }
 
 class MyPriorityQueue {
-    Pair[] queue;
+    Pair[] maxHeap;
     int length;
 
     public MyPriorityQueue(int size) {
-        queue = new Pair[size];
+        maxHeap = new Pair[size];
         length = 0;
     }
 
     public boolean insertWithPriority(Pair pair) {
-        if (length < queue.length) {
-            queue[length] = pair;
+        if (length < maxHeap.length) {
+            maxHeap[length] = pair;
 
             int last = length;
 
-            while ((last / 2) >= 0) {
-                Pair parentYVal = queue[last / 2];
+            while (((last - 1) / 2) >= 0) {
+                Pair parentYVal = maxHeap[(last - 1) / 2];
 
-                if (parentYVal.y >= pair.y)
+                if (parentYVal.occurance >= pair.occurance) {
                     break;
+                }
 
-                Pair temp = queue[last];
-                queue[last] = parentYVal;
-                queue[last / 2] = temp;
+                Pair temp = maxHeap[last];
+                maxHeap[last] = parentYVal;
+                maxHeap[(last - 1) / 2] = temp;
 
-                last /= 2;
+                last = (last - 1) / 2;
             }
             length++;
             return true;
@@ -65,41 +68,43 @@ class MyPriorityQueue {
     }
 
     public Pair pullHighestPriority() {
-        Pair top = queue[0];
+        Pair top = maxHeap[0];
         length--;
 
-        queue[0] = queue[length];
-        queue[length] = top;
+        maxHeap[0] = maxHeap[length];
+        maxHeap[length] = top;
 
-        bubbleDown(queue, 0, length);
+        bubbleDown(0, length);
+
         return top;
     }
 
-    public void bubbleDown(Pair[] heap, int last, int length) {
-        int current = last, leftChildIndex = (2 * last) + 1, rightChildIndex = (2 * last) + 2;
+    public void bubbleDown(int first, int length) {
+        int largest = first, leftChildIndex = (2 * first) + 1, rightChildIndex = (2 * first) + 2;
 
-        if (rightChildIndex < length && heap[rightChildIndex].y > heap[current].y)
-            current = rightChildIndex;
+        if (rightChildIndex < length && maxHeap[rightChildIndex].occurance > maxHeap[largest].occurance) {
+            largest = rightChildIndex;
+        }
 
-        if (leftChildIndex < length && heap[leftChildIndex].y > heap[current].y)
-            current = leftChildIndex;
+        if (leftChildIndex < length && maxHeap[leftChildIndex].occurance > maxHeap[largest].occurance) {
+            largest = leftChildIndex;
+        }
 
-        if (current != last) {
-            Pair temp = heap[last];
-            heap[last] = heap[current];
-            heap[current] = temp;
+        if (largest != first) {
+            Pair temp = maxHeap[first];
+            maxHeap[first] = maxHeap[largest];
+            maxHeap[largest] = temp;
 
-            bubbleDown(heap, current, length);
+            bubbleDown(largest, length);
         }
     }
 }
 
 class Pair {
-    // x represents numbers, y is occurance
-    public int x, y;
+    public int number, occurance;
 
-    public Pair(int x, int y) {
-        this.x = x;
-        this.y = y;
+    public Pair(int number, int occurance) {
+        this.number = number;
+        this.occurance = occurance;
     }
 }
