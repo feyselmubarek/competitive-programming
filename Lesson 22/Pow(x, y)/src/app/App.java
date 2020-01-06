4ab8d400554bfbca2141b19922f66a801159dec9
Feysel <feyselmubarek@gmail.com>
2020-01-06 19:42:54 +0300

    Power function completed

---
+++ Lesson 22/Pow(x, y)/src/app/App.java
@@ -0,0 +1,28 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println(new App().myPow(2.00000, 214));
+    }
+
+    public double myPow(double x, int n) {
+        double res = getPosPow(x, Math.abs(n));
+
+        return n < 0 ? (1 / res) : res;
+    }
+
+    public double getPosPow(double x, int n) {
+        if (n == 0) {
+            return 1.0;
+        }
+
+        double halfRes = getPosPow(x, (n / 2));
+
+        if (n % 2 != 0) {
+            return halfRes * halfRes * x;
+        }
+
+        return halfRes * halfRes;
+
+    }
+}
