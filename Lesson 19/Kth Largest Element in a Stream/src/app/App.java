468251934041aff21a28703e428c6bb7461b40bd
Feysel <feyselmubarek@gmail.com>
2020-01-07 09:48:55 +0300

    K largest Element in the stream compleled

--- Lesson 19/Kth Largest Element in a Stream/src/app/App.java
+++ Lesson 19/Kth Largest Element in a Stream/src/app/App.java
@@ -2,12 +2,12 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        KthLargest obj = new KthLargest(1, new int[] { 0 });
-        System.out.println(obj.add(-3));
+        KthLargest obj = new KthLargest(2, new int[] { 0 });
+        System.out.println(obj.add(-1));
+        System.out.println(obj.add(1));
         System.out.println(obj.add(-2));
-        // System.out.println(obj.add(-2));
+        // System.out.println(obj.add());
         System.out.println(obj.add(-4));
-        System.out.println(obj.add(0));
-        System.out.println(obj.add(4));
+        System.out.println(obj.add(3));
     }
 }
--- Lesson 19/Kth Largest Element in a Stream/src/app/KthLargest.java
+++ Lesson 19/Kth Largest Element in a Stream/src/app/KthLargest.java
@@ -1,78 +1,43 @@
 // question link: https://leetcode.com/problems/kth-largest-element-in-a-stream/
 package app;
 
+import java.util.PriorityQueue;
+
 public class KthLargest {
-    int k, kthLargestNumber, noOfRemainingElement;
-    int[] maxHeap;
+    PriorityQueue<Integer> minHeap;
+    int capacity;
 
     public KthLargest(int k, int[] nums) {
-        this.k = k;
-        noOfRemainingElement = k;
-        maxHeap = new int[k > nums.length ? k : nums.length];
+        minHeap = new PriorityQueue<Integer>(k);
+        capacity = k;
 
         for (int i = 0; i < nums.length; i++) {
-            maxHeap[i] = nums[i];
-            noOfRemainingElement--;
+            insertIntoMinHeap(nums[i]);
         }
-
-        kthLargestNumber = getKthLargestNumber(maxHeap);
-    }
-
-    private int getKthLargestNumber(int[] maxHeap) {
-        int counterUntilK = 0, kthNumber = 0;
-
-        while (counterUntilK < k) {
-            int currentLastIndex = (maxHeap.length - 1) - counterUntilK;
-            heapify(maxHeap, 0, currentLastIndex, currentLastIndex + 1);
-
-            int currntMax = maxHeap[0];
-            maxHeap[0] = maxHeap[currentLastIndex];
-            maxHeap[currentLastIndex] = currntMax;
-
-            kthNumber = currntMax;
-            counterUntilK++;
-        }
-
-        return kthNumber;
     }
 
     public int add(int val) {
-        if (noOfRemainingElement > 0) {
-            maxHeap[maxHeap.length - 1] = val;
-            kthLargestNumber = getKthLargestNumber(maxHeap);
-            noOfRemainingElement--;
+        if (minHeap.size() == capacity) {
+            if (minHeap.peek() < val) {
+                minHeap.poll();
+                minHeap.add(val);
+            }
 
-            return kthLargestNumber;
+            return minHeap.peek();
         }
 
-        if (val <= kthLargestNumber)
-            return kthLargestNumber;
-
-        int index = maxHeap.length - (k + 1) < 0 ? 0 : maxHeap.length - (k + 1);
-        maxHeap[index] = val;
-        kthLargestNumber = getKthLargestNumber(maxHeap);
-
-        return kthLargestNumber;
+        minHeap.add(val);
+        return minHeap.peek();
     }
 
-    public void heapify(int[] heap, int first, int last, int length) {
-        for (int i = last; i >= first; i--) {
-            int leftChildIndex = (2 * i) + 1, rightChildIndex = (2 * i) + 2;
-            int current = i;
-
-            if (rightChildIndex < length && heap[rightChildIndex] > heap[current])
-                current = rightChildIndex;
-
-            if (leftChildIndex < length && heap[leftChildIndex] > heap[current])
-                current = leftChildIndex;
-
-            if (current != i) {
-                int temp = heap[i];
-                heap[i] = heap[current];
-                heap[current] = temp;
-
-                heapify(heap, current, last, length);
+    public void insertIntoMinHeap(int val) {
+        if (minHeap.size() == capacity) {
+            if (minHeap.peek() < val) {
+                minHeap.poll();
+                minHeap.add(val);
             }
+        } else {
+            minHeap.add(val);
         }
     }
 }
