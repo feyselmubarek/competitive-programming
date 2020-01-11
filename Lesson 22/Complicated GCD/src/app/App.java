6453696d79b9a37bf71945f672f24044898aad62
Feysel <feyselmubarek@gmail.com>
2020-01-11 14:05:10 +0300

    compilcated GCD in progress

---
+++ Lesson 22/Complicated GCD/src/app/App.java
@@ -0,0 +1,12 @@
+// question-link : https://codeforces.com/problemset/problem/664/A
+package app;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.close();
+    }
+}
