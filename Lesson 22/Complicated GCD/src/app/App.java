4a2cdc2c0dbf215d8c119b02fe27f45dc7c2aecf
Feysel <feyselmubarek@gmail.com>
2020-01-12 17:34:49 +0300

    compl. GCD completed

--- Lesson 22/Complicated GCD/src/app/App.java
+++ Lesson 22/Complicated GCD/src/app/App.java
@@ -7,6 +7,13 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
 
+        String[] numbersRange = scanner.nextLine().split(" ");
+        System.out.println(findGCD(numbersRange));
+
         scanner.close();
+    }
+
+    public static String findGCD(String[] strArr) {
+        return strArr[0].equals(strArr[1]) ? strArr[0] : "1";
     }
 }
