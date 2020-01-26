b26707ec4a918611e4a6dcf33912bce4038f12de
Feysel <feyselmubarek@gmail.com>
2020-01-26 14:40:17 +0300

    play with chips solved

---
+++ Lesson 26/Play with Chips/src/app/App.java
@@ -0,0 +1,21 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello Java");
+    }
+
+    public int minCostToMoveChips(int[] chips) {
+        int odd = 0, even = 0;
+
+        for (int i = 0; i < chips.length; i++) {
+            if (chips[i] % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+        }
+
+        return even < odd ? even : odd;
+    }
+}
