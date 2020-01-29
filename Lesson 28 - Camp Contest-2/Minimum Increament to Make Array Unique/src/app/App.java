e56733c1de1c160a27394de019f11e139cf44c23
Feysel <feyselmubarek@gmail.com>
2020-01-30 00:09:52 +0300

    some restructuring

--- Lesson 28 - Camp Contest-2/Minimum Increament to Make Array Unique/src/app/App.java
+++ Lesson 28 - Camp Contest-2/Minimum Increament to Make Array Unique/src/app/App.java
@@ -8,7 +8,23 @@ public class App {
     }
 
     public int minIncrementForUnique(int[] A) {
+        if (A.length == 0) {
+            return 0;
+        }
 
-        return 0;
+        Arrays.sort(A);
+        int holder = A[0];
+        int moves = 0;
+
+        for (int i = 1; i < A.length; i++) {
+            if (holder >= A[i]) {
+                holder++;
+                moves += holder - A[i];
+            } else {
+                holder = A[i];
+            }
+        }
+
+        return moves;
     }
 }
