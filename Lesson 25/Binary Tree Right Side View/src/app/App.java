05de785f309e96328db17384b26904c506d17855
Feysel <feyselmubarek@gmail.com>
2020-02-03 23:37:36 +0300

    Binary Tree RightView Solved

---
+++ Lesson 25/Binary Tree Right Side View/src/app/App.java
@@ -0,0 +1,24 @@
+package app;
+
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello Java");
+    }
+
+    public List<Integer> rightSideView(TreeNode root) {
+
+        return null;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
