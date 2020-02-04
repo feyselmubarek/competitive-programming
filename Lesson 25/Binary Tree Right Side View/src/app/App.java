588dc4e317a64c87770f5fded15837b37c67117e
Feysel <feyselmubarek@gmail.com>
2020-02-04 11:19:07 +0300

    BT right side view solved

--- Lesson 25/Binary Tree Right Side View/src/app/App.java
+++ Lesson 25/Binary Tree Right Side View/src/app/App.java
@@ -1,6 +1,9 @@
 package app;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Stack;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -9,7 +12,35 @@ public class App {
 
     public List<Integer> rightSideView(TreeNode root) {
 
-        return null;
+        if (root == null) {
+            return new ArrayList<>();
+        }
+
+        HashMap<Integer, Integer> levelMap = new HashMap<>();
+
+        // dfs
+        Stack<Pair> stack = new Stack<>();
+
+        stack.push(new Pair(root, 1));
+        levelMap.put(1, root.val);
+
+        while (!stack.isEmpty()) {
+            Pair p = stack.pop();
+
+            if (!levelMap.containsKey(p.level)) {
+                levelMap.put(p.level, p.node.val);
+            }
+
+            if (p.node.left != null) {
+                stack.push(new Pair(p.node.left, p.level + 1));
+            }
+
+            if (p.node.right != null) {
+                stack.push(new Pair(p.node.right, p.level + 1));
+            }
+        }
+
+        return new ArrayList<Integer>(levelMap.values());
     }
 }
 
@@ -20,5 +51,15 @@ class TreeNode {
 
     TreeNode(int x) {
         val = x;
+    }
+}
+
+class Pair {
+    public TreeNode node;
+    public int level;
+
+    public Pair(TreeNode node, int level) {
+        this.node = node;
+        this.level = level;
     }
 }
