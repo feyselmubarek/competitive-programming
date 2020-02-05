4a32d2fc066079e8f33aa049d74bac37daaed27b
Feysel <feyselmubarek@gmail.com>
2020-02-05 09:44:35 +0300

    min cost to climb sloved

--- Lesson 31 - Camp/Min Cost Climbing Stairs/src/app/App.java
+++ Lesson 31 - Camp/Min Cost Climbing Stairs/src/app/App.java
@@ -2,6 +2,18 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        System.out.println(new App().minCostClimbingStairs(new int[] { 10, 15, 20 }));
+    }
+
+    public int minCostClimbingStairs(int[] cost) {
+        return findMin(cost.length, cost, 0);
+    }
+
+    public int findMin(int n, int[] arr, int cost) {
+        for (int i = 2; i < arr.length; i++) {
+            arr[i] += Math.min(arr[i - 1], arr[i - 2]);
+        }
+
+        return Math.min(arr[n - 1], arr[n - 2]);
     }
 }
