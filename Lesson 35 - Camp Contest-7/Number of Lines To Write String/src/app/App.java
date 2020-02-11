f9df2bba53125b72ebc5e2a4bc3a67e5416eb921
Feysel <feyselmubarek@gmail.com>
2020-02-11 15:07:33 +0300

    slight update solved

--- Lesson 35 - Camp Contest-7/Number of Lines To Write String/src/app/App.java
+++ Lesson 35 - Camp Contest-7/Number of Lines To Write String/src/app/App.java
@@ -6,8 +6,7 @@ public class App {
     }
 
     public int[] numberOfLines(int[] widths, String S) {
-        int line_no = 1;
-        int curr_width = 0;
+        int line_no = 1, curr_width = 0;
 
         for (int i = 0; i < S.length(); i++) {
             char c = S.charAt(i);
