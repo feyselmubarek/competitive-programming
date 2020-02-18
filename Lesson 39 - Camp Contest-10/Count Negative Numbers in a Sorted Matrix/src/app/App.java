8cbfbdb1dd4d28a54739d9c9494c9f3e377cd7f0
Feysel <feyselmubarek@gmail.com>
2020-02-18 22:09:24 +0300

    slight adjustment

--- Lesson 39 - Camp Contest-10/Count Negative Numbers in a Sorted Matrix/src/app/App.java
+++ Lesson 39 - Camp Contest-10/Count Negative Numbers in a Sorted Matrix/src/app/App.java
@@ -7,16 +7,16 @@ public class App {
 
     public int countNegatives(int[][] grid) {
         int count = 0;
-        
-        for(int i = 0; i < grid.length; i++){
-            for(int j = 0; j < grid[i].length; j++){
-                if(grid[i][j] < 0){
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] < 0) {
                     count += grid[i].length - j;
                     break;
                 }
             }
         }
-        
+
         return count;
     }
 }
