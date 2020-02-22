91dc1828deffe90d6daff9ef8d97c7c4e4758aba
Feysel <feyselmubarek@gmail.com>
2020-02-23 00:07:39 +0300

    slight modification on wiggle

--- Lesson 32 - Camp/Wiggle Subsequence/src/app/App.java
+++ Lesson 32 - Camp/Wiggle Subsequence/src/app/App.java
@@ -6,35 +6,32 @@ public class App {
     }
 
     public int wiggleMaxLength(int[] nums) {
-        if(nums.length == 0){
-            return 0;    
+        if (nums.length == 0) {
+            return 0;
         }
-        
+
         boolean isFound = false, wasIncreasing = false;
         int len = 1;
-        
-        for(int i = 1; i < nums.length; i++){
-            if(!isFound && nums[i] != nums[i - 1]){
+
+        for (int i = 1; i < nums.length; i++) {
+            if (!isFound && nums[i] != nums[i - 1]) {
                 isFound = true;
-                
-                if(nums[i - 1] < nums[i]){
+
+                if (nums[i - 1] < nums[i]) {
                     wasIncreasing = true;
-                }else{
-                    wasIncreasing = false;
-                }   
-                
+                }
+
                 len++;
-                
+
                 continue;
             }
-            
-            
-            if((nums[i - 1] < nums[i] && !wasIncreasing) || (nums[i - 1] > nums[i] && wasIncreasing)){
+
+            if ((nums[i - 1] < nums[i] && !wasIncreasing) || (nums[i - 1] > nums[i] && wasIncreasing)) {
                 len++;
                 wasIncreasing = !wasIncreasing;
             }
         }
-        
+
         return len;
     }
 }
