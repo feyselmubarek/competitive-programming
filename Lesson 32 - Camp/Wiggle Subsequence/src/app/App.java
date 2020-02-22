d45915230efcdd1639e0eec2ad881a35c281eff9
Feysel <feyselmubarek@gmail.com>
2020-02-22 23:55:10 +0300

    wiggle sequence solved

---
+++ Lesson 32 - Camp/Wiggle Subsequence/src/app/App.java
@@ -0,0 +1,40 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello Java");
+    }
+
+    public int wiggleMaxLength(int[] nums) {
+        if(nums.length == 0){
+            return 0;    
+        }
+        
+        boolean isFound = false, wasIncreasing = false;
+        int len = 1;
+        
+        for(int i = 1; i < nums.length; i++){
+            if(!isFound && nums[i] != nums[i - 1]){
+                isFound = true;
+                
+                if(nums[i - 1] < nums[i]){
+                    wasIncreasing = true;
+                }else{
+                    wasIncreasing = false;
+                }   
+                
+                len++;
+                
+                continue;
+            }
+            
+            
+            if((nums[i - 1] < nums[i] && !wasIncreasing) || (nums[i - 1] > nums[i] && wasIncreasing)){
+                len++;
+                wasIncreasing = !wasIncreasing;
+            }
+        }
+        
+        return len;
+    }
+}
