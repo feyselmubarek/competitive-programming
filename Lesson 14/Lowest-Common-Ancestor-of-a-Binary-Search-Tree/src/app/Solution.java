ee6d578ae361e8f30be5b7bde70ffcdff6303551
Feysel <feyselmubarek@gmail.com>
2020-03-02 23:08:31 +0300

    LCS in BST optimized

--- Lesson 14/Lowest-Common-Ancestor-of-a-Binary-Search-Tree/src/app/Solution.java
+++ Lesson 14/Lowest-Common-Ancestor-of-a-Binary-Search-Tree/src/app/Solution.java
@@ -1,32 +1,20 @@
 package app;
 
 public class Solution {
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        TreeNode leftNode = null, rightNode = null;
-        if (root == null) {
+    public TreeNode lowestCommonAncestor(TreeNode node, TreeNode p, TreeNode q) {
+        if (node == null) {
             return null;
         }
 
-        if (root.val == p.val || root.val == q.val) {
-            return root;
+        if ((node.val > Math.min(p.val, q.val) && node.val < Math.max(p.val, q.val)) || node.val == p.val
+                || node.val == q.val) {
+            return node;
         }
 
-        if (root.left != null) {
-            leftNode = lowestCommonAncestor(root.left, p, q);
+        if (node.val > p.val && node.val > q.val) {
+            return lowestCommonAncestor(node.left, p, q);
         }
 
-        if (root.right != null) {
-            rightNode = lowestCommonAncestor(root.right, p, q);
-        }
-
-        if (leftNode != null && rightNode != null) {
-            return root;
-        }
-
-        if (leftNode != null) {
-            return leftNode;
-        }
-
-        return rightNode;
+        return lowestCommonAncestor(node.right, p, q);
     }
 }
